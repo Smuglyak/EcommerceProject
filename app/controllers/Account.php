ef444e17b353657a80e6c9067e9c63f55deee441
Alihan Djamankulov <dubashevalihan@gmail.com>
2022-11-20 16:05:44 -0500

    Added account controller

---
+++ app/controllers/Account.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\controllers;
+
+class Account extends \app\core\Controller{
+
+    public function index()
+    {
+        $this->view('Main/index');
+    }
+
+    public function edit(){
+
+    }
+
+    public function checkHistory(){
+        
+    }
+
+}
+
+?>
