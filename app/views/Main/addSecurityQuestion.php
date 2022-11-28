9bed46817d7856837abcd04bb672432a4511f0eb
CJustom <82058588+CraigJustinBalibalos@users.noreply.github.com>
2022-11-27 22:20:08 -0500

    Update addSecurityQuestion.php

--- app/views/Main/addSecurityQuestion.php
+++ app/views/Main/addSecurityQuestion.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <title>Add Sequrity Question</title>
+  <title>Add Security Question</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
@@ -22,7 +22,7 @@
             <div class="form-outline mb-4">
              <label for="question">Question:</label>
               <select name="question" id="question">
-                <optgroup label="Breads">
+                <optgroup label="Questions">
                   <?php 
                     foreach (questions as $key=>$question) {
                       echo "<option value=$key>$question</option>";
