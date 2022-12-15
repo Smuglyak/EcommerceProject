322184231cc3a8b5de589f1f9ab22d5fb975717f
Alihan Djamankulov <dubashevalihan@gmail.com>
2022-12-15 12:14:58 -0500

    Fix

--- app/core/init.php
+++ app/core/init.php
@@ -4,7 +4,7 @@ if (isset($_GET['lang'])) { //if there is a language choice in the querystring
 	$lang = $_GET['lang']; //use this language
 	setcookie("lang", $lang, 0, '/'); //set a cookie
 } else
-	$lang = (isset($_COOKIE["lang"]) ? $_COOKIE["lang"] : 'en'); //from cookie or default
+	$lang = (isset($_COOKIE["lang"]) ? $_COOKIE["lang"] : 'en_CA'); //from cookie or default
 //extract the root language from the complete locale to use with strftime
 $rootlang = preg_split('/_/', $lang);
 $rootlang = (is_array($rootlang) ? $rootlang[0] : $rootlang);
